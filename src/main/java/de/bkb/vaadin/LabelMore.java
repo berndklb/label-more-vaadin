1a44a97c73c88247fcbcde160cdb563a2ec2e52c
Bernd Kleine-Börger <Bernd.Kleine-Boerger@LMIS.de>
2019-09-17 15:09:39 +0200

    init label more with polymer version

---
+++ src/main/java/de/bkb/vaadin/LabelMore.java
@@ -0,0 +1,41 @@
+package de.bkb.vaadin;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@Tag("label-more")
+@JsModule("label-more/label-more.js")
+@NpmPackage(value = "label-more", version = "1.0.0")
+/*
+ If you wish to include your own JS modules in the add-on jar, add the module
+ files to './src/main/resources/META-INF/resources/frontend' and insert an
+ annotation @JsModule("./my-module.js") here.
+*/
+public class LabelMore extends Component {
+
+	public LabelMore() {
+	}
+	
+	public void setContentHeight(String contentHeight) {
+		getElement().setProperty("contentheight", contentHeight);
+	}
+	
+	public void setButtonCaptions(String readMore, String readLess) {
+		getElement().setProperty("readmorecaption", readMore);
+		getElement().setProperty("readlesscaption", readLess);
+	}
+	
+	public void setHtml(String html) {
+		StringBuffer htmlBuffer = new StringBuffer();
+		htmlBuffer.append("<div>");
+		htmlBuffer.append(html);
+		htmlBuffer.append("</div>");
+		Html htmlComp = new Html(htmlBuffer.toString());
+		getElement().removeAllChildren();
+		getElement().appendChild(htmlComp.getElement());
+	}
+
+}
---
+++ src/test/java/de/bkb/vaadin/View.java
@@ -0,0 +1,17 @@
+package de.bkb.vaadin;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+@CssImport("./styles/colors.css")
+public class View extends Div {
+
+    public View() {
+        LabelMore labelMore = new LabelMore();
+        labelMore.setContentHeight("140");
+        labelMore.setHtml("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+        add(labelMore);
+    }
+}
