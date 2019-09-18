794a72a10a46945115b3288ba5d18415d80b2a2b
Bernd Kleine-Börger <Bernd.Kleine-Boerger@LMIS.de>
2019-09-18 16:12:10 +0200

    set button captions

--- src/test/java/de/bkb/vaadin/View.java
+++ src/test/java/de/bkb/vaadin/View.java
@@ -11,6 +11,7 @@ public class View extends Div {
     public View() {
         LabelMore labelMore = new LabelMore();
         labelMore.setContentHeight("140");
+        labelMore.setButtonCaptions("Mehr lesen", "Weniger lesen");
         labelMore.setHtml("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
         add(labelMore);
     }
